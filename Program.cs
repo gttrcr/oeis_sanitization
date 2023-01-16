d5d01377b6466e5d90748c6e1f67a28738bcfa17
gttrcr <gttrcr@gmail.com>
2023-01-16 19:24:23 +0100

    Move methods

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace oeis_sanitization
 {
@@ -30,177 +28,80 @@ namespace oeis_sanitization
             File.WriteAllText(db_json, str);
         }
 
-        public static void MarkdownPage(string title, string mdFile, List<RDb> list, int numberOfColumns, List<List<string>>? options = null, List<string>? optionsName = null)
-        {
-            StringBuilder md = new StringBuilder();
-            md.AppendLine("---");
-            md.AppendLine("layout: page");
-            md.AppendLine("title: " + title);
-            md.AppendLine("---");
-            md.AppendLine();
-            md.AppendLine("There are " + list.Count + " sequences in the table. Last update is " + File.GetCreationTime("db.json"));
-            md.AppendLine();
-
-            if (list.Count > 0)
-            {
-                md.AppendLine(string.Concat(Enumerable.Repeat("|Number|" + (optionsName != null ? string.Join("|", optionsName) + "|" : ""), numberOfColumns)));
-                md.AppendLine(string.Concat(Enumerable.Repeat("|-", (2 + (optionsName != null ? optionsName.Count : 0)) * numberOfColumns - 1)) + "|");
-
-                int i = 0;
-                for (i = 0; i < list.Count; i++)
-                {
-                    for (int n = 0; n < numberOfColumns && i * numberOfColumns + n < list.Count; n++)
-                    {
-                        md.Append(list[i * numberOfColumns + n].ToString());
-                        if (options != null)
-                            md.Append(string.Join(", ", options[i * numberOfColumns + n]) + "|");
-                    }
-
-                    md.AppendLine();
-                }
-            }
-
-            File.WriteAllText("oeis_mds/" + mdFile + ".md", md.ToString());
-        }
-
-        public static List<string> LinkExtractor(List<string> list)
-        {
-            return LinkExtractor(string.Join("", list));
-        }
-
-        public static List<string> LinkExtractor(string str)
-        {
-            Regex extractDateRegex = new Regex("https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)");
-            return extractDateRegex.Matches(str).Cast<Match>().Select(m => m.Value).ToList();
-        }
-
-        public static List<string> AllLinks(RDb rDb)
-        {
-            List<string> list = new List<string>();
-            list.AddRange(LinkExtractor(rDb.id));
-            list.AddRange(LinkExtractor(rDb.data));
-            list.AddRange(LinkExtractor(rDb.name));
-            list.AddRange(LinkExtractor(rDb.comment));
-            list.AddRange(LinkExtractor(rDb.reference));
-            list.AddRange(LinkExtractor(rDb.link));
-            list.AddRange(LinkExtractor(rDb.formula));
-            list.AddRange(LinkExtractor(rDb.example));
-            list.AddRange(LinkExtractor(rDb.maple));
-            list.AddRange(LinkExtractor(rDb.mathematica));
-            list.AddRange(LinkExtractor(rDb.program));
-            list.AddRange(LinkExtractor(rDb.xref));
-            list.AddRange(LinkExtractor(rDb.offset));
-            list.AddRange(LinkExtractor(rDb.author));
-            list.AddRange(LinkExtractor(rDb.keyword));
-
-            return list;
-        }
-
-        public static List<string> BrokenLinks(List<string> links)
-        {
-            List<string> ret = new List<string>();
-            Parallel.ForEach(links, new ParallelOptions() { MaxDegreeOfParallelism = 8 }, link =>
-            {
-                if (!link.ToLower().Contains("arxiv.org") && !Utils.IsLinkWorking(link))
-                {
-                    Console.WriteLine(link);
-                    ret.Add(link);
-                }
-            });
-
-            return ret;
-        }
-
         public static void Sanitization(List<RDb> oeisDb)
         {
-            int index = 0;
             Console.WriteLine("Broken links...");
-            List<Tuple<RDb, List<string>>> broken = new List<Tuple<RDb, List<string>>>();
-            broken = oeisDb.Select(x => new Tuple<RDb, List<string>>(x, AllLinks(x))).Where(x => x.Item2.Count > 0).ToList();
-            broken = broken.AsParallel().Select(x =>
-            {
-                Console.WriteLine(index++ + "/" + broken.Count);
-                return new Tuple<RDb, List<string>>(x.Item1, BrokenLinks(x.Item2));
-            }).Where(x => x.Item2.Count > 0).ToList();
-            MarkdownPage("Sequences that contains broken links", "broken_links",
-                broken.Select(x => x.Item1).ToList(), //list of sequences
-                1,
-                broken.Select(x => x.Item2.Select(y => "[" + y + "](" + y + ")").ToList()).ToList(),  //list of broken links for every sequence
-                new List<string>() { "Links" });
+            Processes.BrokenLinks(ref oeisDb);
             Console.WriteLine("Done");
 
-            return;
-
-            List<RDb> subset;
             Console.WriteLine("Done deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("done")).ToList();
-            MarkdownPage("Sequences with done deprecated keyword", "done_keyword", subset, 3);
+            Processes.DoneDeprecatedKeyword(ref oeisDb);
             Console.WriteLine("Done");
 
-            Console.WriteLine("Dupe deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("dupe")).ToList();
-            MarkdownPage("Sequences with dupe deprecated keyword", "dupe_keyword", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Huge deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("huge")).ToList();
-            MarkdownPage("Sequences with huge deprecated keyword", "huge_keyword", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Look deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("look")).ToList();
-            MarkdownPage("Sequences with look deprecated keyword", "look_keywords", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Part deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("part")).ToList();
-            MarkdownPage("Sequences with part deprecated keyword", "part_keywords", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Uned sequences...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("uned")).ToList();
-            MarkdownPage("Uned sequences", "uned_sequences", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Obsc deprecated keyword...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("obsc")).ToList();
-            MarkdownPage("Obscure sequences", "obsc_sequences", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Need mode terms...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("more")).ToList();
-            MarkdownPage("Sequences that needs more terms", "need_more_terms", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Allocated...");
-            subset = oeisDb.AsParallel().Where(x => x.name.StartsWith("allocated for")).ToList();
-            MarkdownPage("Allocated sequences", "allocated", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Maple code...");
-            subset = oeisDb.AsParallel().Where(x => x.maple.Count == 0).ToList();
-            MarkdownPage("Sequences that needs maple code", "need_maple", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Mathematica code...");
-            subset = oeisDb.AsParallel().Where(x => x.mathematica.Count == 0).ToList();
-            MarkdownPage("Sequences that needs mathematica code", "need_mathematica", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Some examples...");
-            subset = oeisDb.AsParallel().Where(x => x.example.Count == 0).ToList();
-            MarkdownPage("Sequences that needs examples", "need_examples", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Unknown...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("unkn")).ToList();
-            MarkdownPage("Unknown sequences", "need_knowledge", subset, 3);
-            Console.WriteLine("Done");
-
-            Console.WriteLine("Dead...");
-            subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("dead")).ToList();
-            MarkdownPage("Dead sequences", "dead", subset, 3);
-            Console.WriteLine("Done");
+            //Console.WriteLine("Dupe deprecated keyword...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("dupe")).ToList();
+            //MarkdownPage("Sequences with dupe deprecated keyword", "dupe_keyword", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Huge deprecated keyword...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("huge")).ToList();
+            //MarkdownPage("Sequences with huge deprecated keyword", "huge_keyword", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Look deprecated keyword...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("look")).ToList();
+            //MarkdownPage("Sequences with look deprecated keyword", "look_keywords", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Part deprecated keyword...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("part")).ToList();
+            //MarkdownPage("Sequences with part deprecated keyword", "part_keywords", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Uned sequences...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("uned")).ToList();
+            //MarkdownPage("Uned sequences", "uned_sequences", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Obsc deprecated keyword...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("obsc")).ToList();
+            //MarkdownPage("Obscure sequences", "obsc_sequences", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Need mode terms...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("more")).ToList();
+            //MarkdownPage("Sequences that needs more terms", "need_more_terms", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Allocated...");
+            //subset = oeisDb.AsParallel().Where(x => x.name.StartsWith("allocated for")).ToList();
+            //MarkdownPage("Allocated sequences", "allocated", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Maple code...");
+            //subset = oeisDb.AsParallel().Where(x => x.maple.Count == 0).ToList();
+            //MarkdownPage("Sequences that needs maple code", "need_maple", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Mathematica code...");
+            //subset = oeisDb.AsParallel().Where(x => x.mathematica.Count == 0).ToList();
+            //MarkdownPage("Sequences that needs mathematica code", "need_mathematica", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Some examples...");
+            //subset = oeisDb.AsParallel().Where(x => x.example.Count == 0).ToList();
+            //MarkdownPage("Sequences that needs examples", "need_examples", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Unknown...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("unkn")).ToList();
+            //MarkdownPage("Unknown sequences", "need_knowledge", subset, 3);
+            //Console.WriteLine("Done");
+            //
+            //Console.WriteLine("Dead...");
+            //subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("dead")).ToList();
+            //MarkdownPage("Dead sequences", "dead", subset, 3);
+            //Console.WriteLine("Done");
 
             //Console.WriteLine("Mathematica error...");
             //Link link = new Link();
---
+++ Sanitization.cs
@@ -0,0 +1,112 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace oeis_sanitization
+{
+    public static class Processes
+    {
+        private static void MarkdownPage(string title, string mdFile, List<RDb> list, int numberOfColumns, List<List<string>>? options = null, List<string>? optionsName = null)
+        {
+            StringBuilder md = new StringBuilder();
+            md.AppendLine("---");
+            md.AppendLine("layout: page");
+            md.AppendLine("title: " + title);
+            md.AppendLine("---");
+            md.AppendLine();
+            md.AppendLine("There are " + list.Count + " sequences in the table. Last update is " + File.GetCreationTime("db.json"));
+            md.AppendLine();
+
+            if (list.Count > 0)
+            {
+                md.AppendLine(string.Concat(Enumerable.Repeat("|Number|" + (optionsName != null ? string.Join("|", optionsName) + "|" : ""), numberOfColumns)));
+                md.AppendLine(string.Concat(Enumerable.Repeat("|-", (2 + (optionsName != null ? optionsName.Count : 0)) * numberOfColumns - 1)) + "|");
+
+                int i = 0;
+                for (i = 0; i < list.Count; i++)
+                {
+                    for (int n = 0; n < numberOfColumns && i * numberOfColumns + n < list.Count; n++)
+                    {
+                        md.Append(list[i * numberOfColumns + n].ToString());
+                        if (options != null)
+                            md.Append(string.Join(", ", options[i * numberOfColumns + n]) + "|");
+                    }
+
+                    md.AppendLine();
+                }
+            }
+
+            File.WriteAllText("oeis_mds/" + mdFile + ".md", md.ToString());
+        }
+
+        private static List<string> LinkExtractor(string str)
+        {
+            Regex extractDateRegex = new Regex("https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)");
+            return extractDateRegex.Matches(str).Cast<Match>().Select(m => m.Value).ToList();
+        }
+
+        private static List<string> LinkExtractor(List<string> list)
+        {
+            return LinkExtractor(string.Join("", list));
+        }
+
+        private static List<string> AllLinks(RDb rDb)
+        {
+            List<string> list = new List<string>();
+            list.AddRange(LinkExtractor(rDb.id));
+            list.AddRange(LinkExtractor(rDb.data));
+            list.AddRange(LinkExtractor(rDb.name));
+            list.AddRange(LinkExtractor(rDb.comment));
+            list.AddRange(LinkExtractor(rDb.reference));
+            list.AddRange(LinkExtractor(rDb.link));
+            list.AddRange(LinkExtractor(rDb.formula));
+            list.AddRange(LinkExtractor(rDb.example));
+            list.AddRange(LinkExtractor(rDb.maple));
+            list.AddRange(LinkExtractor(rDb.mathematica));
+            list.AddRange(LinkExtractor(rDb.program));
+            list.AddRange(LinkExtractor(rDb.xref));
+            list.AddRange(LinkExtractor(rDb.offset));
+            list.AddRange(LinkExtractor(rDb.author));
+            list.AddRange(LinkExtractor(rDb.keyword));
+
+            return list;
+        }
+
+        private static List<string> BrokenLinks(List<string> links)
+        {
+            List<string> ret = new List<string>();
+            Parallel.ForEach(links, new ParallelOptions() { MaxDegreeOfParallelism = 8 }, link =>
+            {
+                if (!link.ToLower().Contains("arxiv.org") && !Utils.IsLinkWorking(link))
+                {
+                    Console.WriteLine(link);
+                    ret.Add(link);
+                }
+            });
+
+            return ret;
+        }
+
+        public static void BrokenLinks(ref List<RDb> oeisDb)
+        {
+            int index = 0;
+            List<Tuple<RDb, List<string>>> broken = new List<Tuple<RDb, List<string>>>();
+            broken = oeisDb.Select(x => new Tuple<RDb, List<string>>(x, AllLinks(x))).Where(x => x.Item2.Count > 0).ToList();
+            broken = broken.AsParallel().Select(x =>
+            {
+                Console.WriteLine(index++ + "/" + broken.Count);
+                return new Tuple<RDb, List<string>>(x.Item1, BrokenLinks(x.Item2));
+            }).Where(x => x.Item2.Count > 0).ToList();
+            MarkdownPage("Sequences that contains broken links", "broken_links",
+                broken.Select(x => x.Item1).ToList(), //list of sequences
+                1,
+                broken.Select(x => x.Item2.Select(y => "[" + y + "](" + y + ")").ToList()).ToList(),  //list of broken links for every sequence
+                new List<string>() { "Links" });
+        }
+
+        public static void DoneDeprecatedKeyword(ref List<RDb> oeisDb)
+        {
+            List<RDb> subset = oeisDb.AsParallel().Where(x => x.keyword.Contains("done")).ToList();
+            MarkdownPage("Sequences with done deprecated keyword", "done_keyword", subset, 3);
+        }
+    }
+}
